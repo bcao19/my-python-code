d5efc84f5c958470b8e728c74703600ad00b6aa9
bcao19 <bcao19@gmail.com>
2021-07-13 15:27:53 +0800

    dfda

--- plot_XUV.py
+++ plot_XUV.py
@@ -2,7 +2,7 @@
 Author       : caobin
 Date         : 2021-07-13 10:08:30
 LastEditors  : caobin
-LastEditTime : 2021-07-13 15:04:09
+LastEditTime : 2021-07-13 15:17:21
 FilePath     : \my-python-code\plot_XUV.py
 '''
 
@@ -11,6 +11,7 @@ FilePath     : \my-python-code\plot_XUV.py
 # this code plot the XUV data from mdsplus
 
 
+from scipy import signal
 import MDSplus as mds
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,10 +32,16 @@ else:
     end = float(end)
 
 
-[t, x] = get.data('PXUV1', shot)
+[t, x] = get.data('Pxuv1', shot)
 index = np.where((t>=begin)&(t<=end))
 t = t[index]
 
-for i in range(2, 65):
+Pxuv = np.zeros([64, len(t)], dtype=float)
+Pxuv[0, :] = x[index]
+
+for i in range(1, 64):
     singal_name = 'Pxuv'
-    
+    signal_name = signal_name+str(i+1)
+    x = get.data1(singal_name, shot)
+    Pxuv[i, :] = x[index]
+
