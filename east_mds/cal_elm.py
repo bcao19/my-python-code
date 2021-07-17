fe873acfee86ecfefd259ae057f2c81ac10b2a0b
bcao19 <bcao19@gmail.com>
2021-07-17 14:26:01 +0800

    add dw to cal_elm

--- east_mds/cal_elm.py
+++ east_mds/cal_elm.py
@@ -4,7 +4,7 @@ Author: caobin
 Date: 2021-07-13 22:27:24
 Github: https://github.com/bcao19
 LastEditors  : caobin
-LastEditTime : 2021-07-15 16:00:23
+LastEditTime : 2021-07-17 14:25:42
 '''
 
 #!/home/ASIPP/caobin/anaconda3/bin/python
@@ -78,6 +78,35 @@ def find_base(x, win, percent=90):
 
 
 
+
+def cal_dw(t, x, threshold=1):
+    dx1 = x[1 : -1]-x[: -2]
+    dx2 = x[1 : -1]-x[2 :]
+    index1 = np.where((dx1>=0) & (dx2>0))
+    index1 = index1[0]+1
+    index2 = np.where((dx1<=0) & (dx2<0))
+    index2 = index2[0]+1
+    dw = np.zeros(len(index1))
+    for i in range(len(index1)):
+        index = np.where(index2>index1[i])
+        if len(index[0])>0:
+            index = index[0][0]
+            up_index = index1[i]
+            low_index = index2[index]
+            dw[i] = (x[up_index]-x[low_index])/1e3
+
+    t = t[index1]
+    index = np.where(dw>threshold)
+    t = t[index]
+    dw = dw[index]
+
+    return t, dw
+
+
+
+
+
+
 if __name__ == '__main__':
 
     signal = input('Input the signal: ')
@@ -114,53 +143,121 @@ if __name__ == '__main__':
         onlybase = -1
     else:
         onlybase = int(onlybase)
+    dw_threshold = input('Input the dw_threshold: ')
+    if dw_threshold == "":
+        dw_threshold = 0
+    else:
+        dw_threshold = float(dw_threshold)
 
 
-    [t, x] = get.data(signal, shot, tree=tree, timerange=timerange)
-    win = win//(t[1000]-t[0])
+    
 
-    if onlybase<=0:
-        if onlybase == 0:
-            xbase = find_base(x, 100*win, percent)
+    if dw_threshold == 0:
+        [t, x] = get.data(signal, shot, tree=tree, timerange=timerange)
+        win = win//(t[1000]-t[0])
 
-        n = len(threshold)-1
-        if n==0:
-            threshold.append(max(x))
-            n = 1
+        if onlybase<=0:
+            if onlybase == 0:
+                xbase = find_base(x, 100*win, percent)
 
-        colors = ['r', 'g', 'c', 'm', 'y']
-        plt.figure(figsize=(6, 7))
-        plt.subplot(2, 1, 2)
-        plt.plot(t, x)
+            n = len(threshold)-1
+            if n==0:
+                threshold.append(max(x))
+                n = 1
 
-        if onlybase == 0:
-            plt.plot(t, xbase, color='b', linewidth=2)
-        
-        plt.xlabel('time (s)')
-        plt.ylabel(signal)
-        plt.xlim(begin_time, end_time)
-        plt.subplot(2, 1, 1)
-        plt.ylabel('ELM frequency (Hz)')
-        plt.xlim(begin_time, end_time)
-
-        for i in range(n):
-            
-            index = find_peak(x, win, threshold[i], threshold[i+1], percent)
-            f_elm = cal_f(t[index])
+            colors = ['r', 'g', 'c', 'm', 'y']
+            plt.figure(figsize=(6, 7))
             plt.subplot(2, 1, 2)
-            plt.scatter(t[index], x[index], s=40, facecolors='none', edgecolors=colors[i])
+            plt.plot(t, x)
+
+            if onlybase == 0:
+                plt.plot(t, xbase, color='b', linewidth=2)
+            
+            plt.xlabel('time (s)')
+            plt.ylabel(signal)
+            plt.xlim(begin_time, end_time)
             plt.subplot(2, 1, 1)
-            plt.scatter(t[index], f_elm, s=40, facecolors='none', edgecolors=colors[i])
+            plt.ylabel('ELM frequency (Hz)')
+            plt.xlim(begin_time, end_time)
+
+            for i in range(n):
+                
+                index = find_peak(x, win, threshold[i], threshold[i+1], percent)
+                f_elm = cal_f(t[index])
+                plt.subplot(2, 1, 2)
+                plt.scatter(t[index], x[index], s=40, facecolors='none', edgecolors=colors[i])
+                plt.subplot(2, 1, 1)
+                plt.scatter(t[index], f_elm, s=40, facecolors='none', edgecolors=colors[i])
+
+
+        else:
+            xbase = find_base(x, 100*win, percent)
+            plt.figure
+            plt.plot(t, x)
+            plt.xlabel('time (s)')
+            plt.ylabel(signal)
+            plt.xlim(begin_time, end_time)
+            plt.plot(t, xbase, color='b', linewidth=2)
 
 
     else:
-        xbase = find_base(x, 100*win, percent)
-        plt.figure
-        plt.plot(t, x)
-        plt.xlabel('time (s)')
-        plt.ylabel(signal)
-        plt.xlim(begin_time, end_time)
-        plt.plot(t, xbase, color='b', linewidth=2)
+        [t, x] = get.data(signal, shot, tree=tree, timerange=timerange)
+        win = win//(t[1000]-t[0])
+
+        if onlybase<=0:
+            if onlybase == 0:
+                xbase = find_base(x, 100*win, percent)
+
+            n = len(threshold)-1
+            if n==0:
+                threshold.append(max(x))
+                n = 1
+
+            colors = ['r', 'g', 'c', 'm', 'y']
+            plt.figure(figsize=(6, 9))
+            plt.subplot(2, 1, 3)
+            plt.plot(t, x)
+
+            if onlybase == 0:
+                plt.plot(t, xbase, color='b', linewidth=2)
+            
+            plt.xlabel('time (s)')
+            plt.ylabel(signal)
+            plt.xlim(begin_time, end_time)
+            plt.subplot(2, 1, 2)
+            plt.ylabel('ELM frequency (Hz)')
+            plt.xlim(begin_time, end_time)
+            plt.subplot(2, 1, 1)
+            plt.ylabel(r'ELM $\delta$w (kJ)')
+            plt.xlim(begin_time, end_time)
+
+            [td, dw] = cal_dw(t, x, dw_threshold)
+
+            for i in range(n):
+                
+                index = find_peak(x, win, threshold[i], threshold[i+1], percent)
+                f_elm = cal_f(t[index])
+                dw_index = np.zeros(len(index[0]))
+                for i in range(len(index[0])):
+                    tmp = np.where(td>index[0][i])
+                    if len(tmp[0])>0:
+                        dw_index[i] = tmp[0][0]
+
+                plt.subplot(2, 1, 3)
+                plt.scatter(t[index], x[index], s=40, facecolors='none', edgecolors=colors[i])
+                plt.subplot(2, 1, 2)
+                plt.scatter(t[index], f_elm, s=40, facecolors='none', edgecolors=colors[i])
+                plt.scatter(t[index], dw[dw_index])
+
+
+        else:
+            xbase = find_base(x, 100*win, percent)
+            plt.figure
+            plt.plot(t, x)
+            plt.xlabel('time (s)')
+            plt.ylabel(signal)
+            plt.xlim(begin_time, end_time)
+            plt.plot(t, xbase, color='b', linewidth=2)
 
 
 
