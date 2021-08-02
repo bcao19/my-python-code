af0b481b549e8f4c4873d5675a6d0dbe79a014b8
bcao19 <bcao19@gmail.com>
2021-08-02 08:09:22 +0800

    Update plot_east.py

--- plot_east.py
+++ plot_east.py
@@ -262,14 +262,14 @@ def plot_data():
                     
                     plt.rcParams['xtick.direction'] = 'in'#将x周的刻度线方向设置向内
                     plt.rcParams['ytick.direction'] = 'in'#将y轴的刻度方向设置向内
-                    plt.tick_params(top='on',bottom='on',left='on',right='on',labelleft='on',direction='in')
+                    plt.tick_params(top='on',bottom='on',left='on',right='on',labelleft='on',direction='in', labelbottom='off')
                     # if (n-i)&1:
                     #     plt.tick_params(top='on',bottom='on',left='on',right='on',labeltop='off',labelbottom='off',labelleft='off',labelright='on')
                     #     ax1.yaxis.set_label_position("right") 
                     # else:
                     #     plt.tick_params(top='on',bottom='on',left='on',right='on', labeltop='off',labelbottom='off',labelleft='on',labelright='off')
                     if n == 1:
-                        plt.tick_params(top='on',bottom='on',left='on',right='on',labelleft='on',direction='in')
+                        plt.tick_params(top='on',bottom='on',left='on',right='on',labelleft='on', labelbottom='on',direction='in')
                     
                     plt.subplots_adjust(wspace =0, hspace =0.03*n)
                     ax1.ticklabel_format(style='sci', scilimits=(-1,2), axis='y')
@@ -329,7 +329,7 @@ def plot_data():
                     #     ax2.yaxis.set_label_position("right") 
                     # else:
                     #     plt.tick_params(top='on',bottom='on',left='on',right='on',labeltop='off',labelbottom='off',labelleft='on',labelright='off')
-                    plt.tick_params(top='on',bottom='on',left='on',right='on',labelleft='on',direction='in')
+                    plt.tick_params(top='on',bottom='on',left='on',right='on',labelleft='on',direction='in', labelbottom='off')
                     plt.subplots_adjust(wspace =0, hspace =0.03*n)
                     ax2.ticklabel_format(style='sci', scilimits=(-1,2), axis='y')
                     plt.ylabel(slabel)
