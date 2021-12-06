cb2d31a62b5d45312630cc7ea68f2e592c0e65f4
bcao19 <bcao19@gmail.com>
2021-12-06 15:14:04 +0800

    ok

--- casmooc.py
+++ casmooc.py
@@ -2,8 +2,8 @@
 Author       : caobin
 Date         : 2021-12-02 15:20:08
 LastEditors  : caobin
-LastEditTime : 2021-12-02 15:26:17
-FilePath     : \autod:\bcao19\my-python-code\casmooc.py
+LastEditTime : 2021-12-02 16:00:15
+FilePath     : \undefinedd:\bcao19\my-python-code\casmooc.py
 '''
 
 # -*- coding: utf-8 -*-
@@ -85,3 +85,73 @@ for y in y_location:
             time.sleep(3)
             auto.computer_ctrl_w()
             time.sleep(3)
+
+
+for i in range(0, 10):
+    time.sleep(3)
+    auto.computer_prtsc('Screencap.png')
+    auto.computer_matchImgClick('Screencap.png', 'xyy.png')
+
+    for y in y1_location:
+    for x in x_location:
+        time.sleep(3)
+        auto.computer_click(x, y)
+        time.sleep(3)
+        auto.computer_prtsc('Screencap.png')
+        if auto.computer_if_matchImg('Screencap.png', 'start.png'):
+            auto.computer_matchImgClick('Screencap.png', 'start.png')
+            time.sleep(3)
+            auto.computer_prtsc('Screencap.png')
+            if auto.computer_if_matchImg('Screencap.png', 'ksxx.png'):
+                auto.computer_matchImgClick('Screencap.png', 'ksxx.png')
+            time.sleep(600)
+            auto.computer_ctrl_w()
+            time.sleep(3)
+            auto.computer_ctrl_w()
+            
+        else:
+            time.sleep(3)
+            auto.computer_ctrl_w()
+            
+            
+    
+    time.sleep(3)
+    auto.computer_page_down()
+
+    for y in y_location:
+        for x in x_location:
+            time.sleep(3)
+            auto.computer_click(x, y)
+            auto.computer_prtsc('Screencap.png')
+            if auto.computer_if_matchImg('Screencap.png', 'start.png'):
+                auto.computer_matchImgClick('Screencap.png', 'start.png')
+                time.sleep(3)
+                auto.computer_prtsc('Screencap.png')
+                if auto.computer_if_matchImg('Screencap.png', 'ksxx.png'):
+                    auto.computer_matchImgClick('Screencap.png', 'ksxx.png')
+                time.sleep(600)
+                auto.computer_ctrl_w()
+                time.sleep(3)
+                auto.computer_ctrl_w()
+                time.sleep(3)
+                
+                
+    time.sleep(3)
+    auto.computer_page_down()
+
+    for y in y_location:
+        for x in x_location:
+            time.sleep(3)
+            auto.computer_click(x, y)
+            auto.computer_prtsc('Screencap.png')
+            if auto.computer_if_matchImg('Screencap.png', 'start.png'):
+                auto.computer_matchImgClick('Screencap.png', 'start.png')
+                time.sleep(3)
+                auto.computer_prtsc('Screencap.png')
+                if auto.computer_if_matchImg('Screencap.png', 'ksxx.png'):
+                    auto.computer_matchImgClick('Screencap.png', 'ksxx.png')
+                time.sleep(600)
+                auto.computer_ctrl_w()
+                time.sleep(3)
+                auto.computer_ctrl_w()
+                time.sleep(3)
