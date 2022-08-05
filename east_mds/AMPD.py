fa4c02e8e92eb795c232074ccfa3ced2ecf5b6e4
bcao19 <bcao19@gmail.com>
2022-08-05 08:20:21 +0800

    Create AMPD.py

---
+++ east_mds/AMPD.py
@@ -0,0 +1,33 @@
+'''
+Author       : caobin
+Date         : 2022-08-05 08:14:51
+LastEditors  : caobin
+LastEditTime : 2022-08-05 08:16:58
+FilePath     : \my-python-code\east_mds\AMPD.py
+'''
+
+
+import numpy as np
+
+def AMPD(data):
+    """
+    实现AMPD算法
+    :param data: 1-D numpy.ndarray 
+    :return: 波峰所在索引值的列表
+    """
+    p_data = np.zeros_like(data, dtype=np.int32)
+    count = data.shape[0]
+    arr_rowsum = []
+    for k in range(1, count // 2 + 1):
+        row_sum = 0
+        for i in range(k, count - k):
+            if data[i] > data[i - k] and data[i] > data[i + k]:
+                row_sum -= 1
+        arr_rowsum.append(row_sum)
+    min_index = np.argmin(arr_rowsum)
+    max_window_length = min_index
+    for k in range(1, max_window_length + 1):
+        for i in range(k, count - k):
+            if data[i] > data[i - k] and data[i] > data[i + k]:
+                p_data[i] += 1
+    return np.where(p_data == max_window_length)[0]
