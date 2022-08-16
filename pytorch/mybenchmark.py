95cd9be337441f320d263dc3f1d8197a12a8f95a
bcao19 <bcao19@gmail.com>
2022-08-16 08:40:45 +0800

    GPU

--- pytorch/mybenchmark.py
+++ pytorch/mybenchmark.py
@@ -1,10 +1,5 @@
-'''
-Author       : caobin
-Date         : 2022-08-15 09:36:43
-LastEditors  : caobin
-LastEditTime : 2022-08-15 12:59:03
-FilePath     :\bcao19\my-python-code\pytorch\mybenchmark.py
-'''
+
+
 
 import torch
 from torch.utils import benchmark
@@ -15,6 +10,9 @@ n = 1024 * 16
 
 a = torch.randn(n, n).type(typ).cuda()
 b = torch.randn(n, n).type(typ).cuda()
+
+# a = torch.randn(n, n).type(typ)
+# b = torch.randn(n, n).type(typ)
 
 t = benchmark.Timer(stmt='a @ b', globals={'a':a, 'b':b})
 
