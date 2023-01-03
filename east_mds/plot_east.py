e03aecd16e3249d56dffee85793a8eda78c5c453
bcao19 <bcao19@gmail.com>
2023-01-03 16:39:33 +0800

    Update plot_east.py

--- east_mds/plot_east.py
+++ east_mds/plot_east.py
@@ -279,7 +279,7 @@ def plot_data():
                     
                 ax1.plot(t, y, color=color, label=str(shot))
                 
-                plt.rcParams.update({'font.size': 20})
+                
                 ax1.legend()
                 
 
